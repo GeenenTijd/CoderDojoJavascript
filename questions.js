6c83fa9973f24db3399e2e507c7b5fd9b6341672
Glenn Geenen <glenn@geenentijd.be>
2014-09-07 14:01:14 +0200

    Load Correct Questions Fix #11

--- questions.js
+++ questions.js
@@ -1,22 +1,28 @@
-include('questions', ['vars.questions', 'ifelse.questions', 'loops.questions', 'functions.questions'], function (varq, ifelseq, loopsq, functionsq) {
-	'use strict';
-
-	var query = window.location.search.substring(1);
-	var vars = query.split("&");
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split("=");
-		if (pair[0] === 'group') {
-
-			if (pair[1] === 'variables') {
-				return varq;
-			} else if (pair[1] === 'ifelse') {
-				return ifelseq;
-			} else if (pair[1] === 'loops') {
-				return loopsq;
-			} else if (pair[1] === 'functions') {
-				return functionsq;
-			}
-		}
-	}
-	return null;
-});
+(function () {
+    'use strict';
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === 'group') {
+            if (pair[1] === 'variables') {
+                include(['vars.questions'], function (varq) {
+                    return varq;
+                });
+            } else if (pair[1] === 'ifelse') {
+                include(['ifelse.questions'], function (ifelseq) {
+                    return ifelseq;
+                });
+            } else if (pair[1] === 'loops') {
+                include(['loops.questions'], function (loopsq) {
+                    return loopsq;
+                });
+            } else if (pair[1] === 'functions') {
+                include(['functions.questions'], function (functionsq) {
+                    return functionsq;
+                });
+            }
+        }
+    }
+    return null;
+})();
