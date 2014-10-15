791a63d09161618af547676cd9ad84be80493c5b
Glenn Geenen <glenn@geenentijd.be>
2014-10-15 19:10:35 +0200

    Add loops questions

--- app/scripts/questions/loops.js
+++ app/scripts/questions/loops.js
@@ -2,7 +2,7 @@
 
     var while1 = {
         title: 'While',
-        task: 'Wat denk jij dat tekst gaat zijn? Test de code.',
+        task: 'Wat denk jij dat tekst gaat zijn? Test of je gelijk hebt.',
         templateurl: 'views/loops/while1.html',
         code: 'var x = 0;\nvar tekst = \'A\';\n\nwhile(x < 3) {\n\ttekst = tekst + \'A\';\n\tx = x + 1;\n}',
         validate: function (code, next) {
@@ -40,9 +40,74 @@
         }
     };
 
+    var while3 = {
+        title: 'While',
+        task: 'Welke waarde heeft x op het einde? Test of je gelijk hebt.',
+        templateurl: 'views/loops/while3.html',
+        code: 'var x = 0;\nwhile(x < 10) {\n\tx += 3;\n}',
+        validate: function (code, next) {
+            try {
+                var result = eval(code);
+                if (typeof x === 'number') {
+                    next(null, 'x is ' + x);
+                } else {
+                    next('Oei oei, er ging iets mis.');
+                }
+            } catch (e) {
+                next(e.message);
+            }
+        }
+    };
+
+    var while4 = {
+        title: 'While',
+        task: 'Vul de while in zodat x 4 wordt.',
+        templateurl: 'views/loops/while3.html',
+        code: 'var x = 0;\nwhile( ) {\n\tx += 2;\n}',
+        validate: function (code, next) {
+            try {
+                var result = eval(code);
+                if (typeof x === 'number') {
+                    if (x === 4) {
+                        next(null, 'x is ' + x);
+                    } else {
+                        next('x is ' + x);
+                    }
+                } else {
+                    next('Oei oei, er ging iets mis.');
+                }
+            } catch (e) {
+                next(e.message);
+            }
+        }
+    };
+
+    var while5 = {
+        title: 'While',
+        task: 'Vul de while in zodat tekst \'XYXY\' wordt.',
+        templateurl: 'views/loops/while5.html',
+        code: 'var tekst = \'\';\nwhile( ) {\n\ttekst += \'XY\';\n}',
+        validate: function (code, next) {
+            try {
+                var result = eval(code);
+                if (typeof tekst === 'string') {
+                    if (tekst === 'XYXY') {
+                        next(null, 'tekst is ' + tekst);
+                    } else {
+                        next('tekst is ' + tekst);
+                    }
+                } else {
+                    next('Oei oei, er ging iets mis.');
+                }
+            } catch (e) {
+                next(e.message);
+            }
+        }
+    };
+
     var for1 = {
         title: 'For',
-        task: 'Wat denk jij dat tekst gaat zijn? Test de code.',
+        task: 'Wat denk jij dat tekst gaat zijn? Test of je gelijk hebt.',
         templateurl: 'views/loops/for1.html',
         code: 'var tekst = \'A\';\n\nfor(var i = 0; i < 3; ++i) {\n\ttekst = tekst + \'A\';\n}',
         validate: function (code, next) {
@@ -62,7 +127,7 @@
     var for2 = {
         title: 'For',
         task: 'Verander de code zodat tekst AAA wordt.',
-        templateurl: 'views/loops/while1.html',
+        templateurl: 'views/loops/for2.html',
         code: 'var tekst = \'A\';\n\nfor(var i = 0; i < 3; ++i) {\n\ttekst = tekst + \'A\';\n}',
         validate: function (code, next) {
             try {
@@ -80,6 +145,25 @@
         }
     };
 
+    var for3 = {
+        title: 'For',
+        task: 'Welke waarde heeft x op het einde? Test of je gelijk hebt.',
+        templateurl: 'views/loops/while3.html',
+        code: 'var x = 0;\nfor(var i = 0; i < 3; ++i) {\n\tx += 2;\n}',
+        validate: function (code, next) {
+            try {
+                var result = eval(code);
+                if (typeof x === 'number') {
+                    next(null, 'x is ' + x);
+                } else {
+                    next('Oei oei, er ging iets mis.');
+                }
+            } catch (e) {
+                next(e.message);
+            }
+        }
+    };
+
     function loopsQuestions() {
         function getQuestion(question) {
             if (question === 1) {
@@ -87,9 +171,17 @@
             } else if (question === 2) {
                 return while2;
             } else if (question === 3) {
-                return for1;
+                return while3;
             } else if (question === 4) {
+                return while4;
+            } else if (question === 5) {
+                return while5;
+            } else if (question === 6) {
+                return for1;
+            } else if (question === 7) {
                 return for2;
+            } else if (question === 8) {
+                return for3;
             }
             return null;
         }
